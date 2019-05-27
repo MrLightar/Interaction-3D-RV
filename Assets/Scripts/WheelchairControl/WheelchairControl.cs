58b7113ee1ac550be47f5c8021da55a94c97b089
Nicolas <nicolas.marin26@gmail.com>
2019-05-27 07:33:08 -0400

    Scene démo

---
+++ Assets/Scripts/WheelchairControl/WheelchairControl.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelchairControl : MonoBehaviour
+{
+    public WheelInfo motorWheel; // the information about each individual axle
+    public float maxMotorTorque; // maximum torque the motor can apply to wheel
+    public float maxSteeringAngle; // maximum steer angle the wheel can have
+
+    private Rigidbody rb;
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        //foreach (WheelInfo wheelInfo in wheelInfos)
+        //{
+        //    if (wheelInfo.steering)
+        //    {
+        //        wheelInfo.leftWheel.steerAngle = steering;
+        //        wheelInfo.rightWheel.steerAngle = steering;
+        //    }
+        //    if (wheelInfo.motor)
+        //    {
+        //        wheelInfo.leftWheel.motorTorque = motor;
+        //        wheelInfo.rightWheel.motorTorque = motor;
+        //    }
+        //}
+        if(motor != 0)
+        {
+            motorWheel.rightWheel.motorTorque = motor;
+            motorWheel.leftWheel.motorTorque = motor;
+        }
+
+        //if(steering > 0)
+        //{
+        //    motorWheel.rightWheel.motorTorque = steering;
+        //}
+        //else
+        //{
+        //    if(steering < 0)
+        //    {
+        //        motorWheel.leftWheel.motorTorque = steering;
+        //    } else
+        //    {
+        //    }
+        //}
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //rb.velocity = Vector3.zero;
+            //motorWheel.rightWheel.motorTorque = -motorWheel.rightWheel.motorTorque;
+            //motorWheel.leftWheel.motorTorque = -motorWheel.rightWheel.motorTorque;
+        }
+
+        
+    }
+}
+
+[System.Serializable]
+public class WheelInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; // is this wheel attached to motor?
+    public bool steering; // does this wheel apply steer angle?
+    public bool rightWheelMotor = false;
+    public bool leftWheelMotor = false;
+}
+
+
--- Assets/Scripts/WheelchairControl/WheelchairController.cs
+++ Assets/Scripts/WheelchairControl/WheelchairController.cs
@@ -1,56 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof (CharacterController))]
-public class WheelchairController : MonoBehaviour {
+[RequireComponent(typeof(CharacterController))]
+public class WheelchairController : MonoBehaviour
+{
 
-	public float moveSpeed = 0.65f;
+    public float moveSpeed = 0.65f;
 
-	[SerializeField] private float turnSpeed = 2.5f;
-	[SerializeField] private float slopeResistance = 25;
-	[SerializeField] private float stickToGroundForce = 10;
-    [SerializeField] private Rigidbody rightWheel;
-    [SerializeField] private Rigidbody leftWheel;
+    [SerializeField] private float turnSpeed = 0.75f;
+    [SerializeField] private float slopeResistance = 25;
+    [SerializeField] private float stickToGroundForce = 10;
 
     private CharacterController cc;
 
-	private Vector3 velocity;
+    private Vector3 velocity;
 
 
-	// Use this for initialization
-	void Start () {
-		cc = GetComponent<CharacterController>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-        //transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed, 0);
-
-        //Vector3 desiredMove = transform.forward * Input.GetAxis("Vertical") * moveSpeed;
-
-        //RaycastHit hitInfo;
-        //Physics.SphereCast(transform.position, cc.radius, Vector3.down, out hitInfo, cc.height / 2f);
-        //desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal);
-
-        //// Slide down slopes
-        //float hitAngle = Vector3.Angle(hitInfo.normal, Vector3.up);
-        //float slopeEffect = Mathf.Clamp01((hitAngle - slopeResistance) / cc.slopeLimit);
-        //Vector3 slideForce = new Vector3(hitInfo.normal.x, -hitInfo.normal.y, hitInfo.normal.z) * slopeEffect;
-        //velocity += Physics.gravity * Time.deltaTime;
-
-        //cc.Move((velocity + desiredMove + slideForce) * Time.deltaTime);
-
-
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-
-
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        Debug.Log(movement);
-        //rightWheel.AddForce(movement * 100.0f);
-        //leftWheel.AddForce(movement * 100.0f);
+    // Use this for initialization
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed, 0);
+
+        Vector3 desiredMove = transform.forward * Input.GetAxis("Vertical") * moveSpeed;
+
+        RaycastHit hitInfo;
+        Physics.SphereCast(transform.position, cc.radius, Vector3.down, out hitInfo, cc.height / 2f);
+        desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal);
+
+        // Slide down slopes
+        float hitAngle = Vector3.Angle(hitInfo.normal, Vector3.up);
+        float slopeEffect = Mathf.Clamp01((hitAngle - slopeResistance) / cc.slopeLimit);
+        Vector3 slideForce = new Vector3(hitInfo.normal.x, -hitInfo.normal.y, hitInfo.normal.z) * slopeEffect;
+        velocity += Physics.gravity * Time.deltaTime;
+
+        cc.Move((velocity + desiredMove + slideForce) * Time.deltaTime);
+
+
+    }
 }
