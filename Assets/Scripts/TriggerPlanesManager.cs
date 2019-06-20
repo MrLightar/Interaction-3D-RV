6c8ca14ac03b3df68100990f90637042cc0a5087
William <william.zerbato@gmail.com>
2019-06-20 16:30:19 -0400

    Found sound effects + organizing texts appearing

---
+++ Assets/Scripts/TriggerPlanesManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPlanesManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> activateCanvas;
+    [SerializeField]
+    private List<GameObject> deactivateCanvas;
+
+    private bool beenActivated = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(beenActivated == false)
+        {
+            beenActivated = true;
+
+            foreach(GameObject canvas in activateCanvas)
+            {
+                canvas.SetActive(true);
+            }
+
+            foreach (GameObject canvas in deactivateCanvas)
+            {
+                canvas.SetActive(false);
+            }
+        }
+    }
+}
