89ff607128fe44f2b79340d0e48716bff2a28ce0
Griwald <griwald13@gmail.com>
2019-06-20 15:49:56 -0400

    Update PlaceMug.cs

--- Assets/Scripts/Apartment/PlaceMug.cs
+++ Assets/Scripts/Apartment/PlaceMug.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
 public class PlaceMug : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
 
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.tag == "Mug") {
-			Debug.Log("Placé");
+			other.gameObject.GetComponent<OVRGrabbable>().enabled = false;
+			other.gameObject.isStatic = true;
+
+			other.transform.position = new Vector3(15.8f, 20.22f, 18.7f);
+			other.transform.eulerAngles = new Vector3(-90, 0, -90);
 		}
 	}
 }
