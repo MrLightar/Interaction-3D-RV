ccee458fa6a031b8bbcb135749b42c741bdfc194
William <william.zerbato@gmail.com>
2019-06-20 19:28:19 -0400

    TextGuide refonte + ptits détails

--- Assets/Scripts/TriggerPlanesManager.cs
+++ Assets/Scripts/TriggerPlanesManager.cs
@@ -13,11 +13,44 @@ public class TriggerPlanesManager : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(beenActivated == false)
+        if(this.tag == "Trigger1")
+        {
+            manageCanvas();
+        }
+        if (this.tag == "Trigger2")
+        {
+            manageCanvas();
+        }
+        if (this.tag == "Trigger3" && GameManager.Instance.coffeeMade == false && GameManager.Instance.isToasted == false)
+        {
+            manageCanvas();
+        }
+        if (this.tag == "Trigger4" && GameManager.Instance.coffeeMade == false && GameManager.Instance.isToasted == false)
+        {
+            manageCanvas();
+        }
+        if (this.tag == "Trigger5" && GameManager.Instance.coffeeMade == true && GameManager.Instance.isToasted == true)
+        {
+            manageCanvas();
+        }
+        if (this.tag == "Trigger6" && GameManager.Instance.coffeeMade == true && GameManager.Instance.isToasted == true)
+        {
+            manageCanvas();
+        }
+        if (this.tag == "Trigger7" && GameManager.Instance.coffeeMade == true && GameManager.Instance.isToasted == true && GameManager.Instance.toothWash == true)
+        {
+            manageCanvas();
+        }
+
+    }
+
+    private void manageCanvas()
+    {
+        if (beenActivated == false)
         {
             beenActivated = true;
 
-            foreach(GameObject canvas in activateCanvas)
+            foreach (GameObject canvas in activateCanvas)
             {
                 canvas.SetActive(true);
             }
